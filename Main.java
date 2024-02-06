49cdbbdf074d73cf78b01ad2874341d19f643ecb
Charles Dierbach <cdierbach@towson.edu>
2024-02-06 14:32:20 -0500

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,40 @@
+// Code for Practicum 19 (Arrays in Java)
+// (TO COMPLETE)
+import java.util.Scanner;
+
+class Main {
+  public static void main(String[] args) {
+    int sum;
+    double avg1, avg2;
+    
+    Scanner input = new Scanner(System.in);
+    
+    // declare an array of integers called nums of size 100
+    // (to complete)
+   
+
+    // create a for loop that populates array nums such that
+    // the first element is 100, the second element 101, etc.
+    // (to complete)
+    
+    
+    // use a for loop to determine the average of all of the
+    // values in array nums and assigns to variable avg1
+    // (to complete)
+    
+
+    // use a for loop to add 5 to every elements in array nums
+    // (to complete)
+    
+    
+    // use a for loop to determine the average of all of the
+    // values in array nums and assigns to variable avg2
+    // (to complete)
+    
+
+    // display results
+    System.out.println("avg1 = " + avg1);
+    System.out.println("avg2 = " + avg2);
+    
+  }
+}
