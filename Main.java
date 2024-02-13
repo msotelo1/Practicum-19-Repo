3b09c616a14a87d43cc377436c9c6178863a32ce
msotelo1 <158190815+msotelo1@users.noreply.github.com>
2024-02-13 01:40:06 -0500

    Matthew Sotelos Update Main.java

--- Main.java
+++ Main.java
@@ -1,40 +1,38 @@
-// Code for Practicum 19 (Arrays in Java)
-// (TO COMPLETE)
 import java.util.Scanner;
 
-class Main {
+public class Main {
   public static void main(String[] args) {
-    int sum;
-    double avg1, avg2;
-    
+    int sum = 0;
+    double avg1 = 0.0, avg2 = 0.0;
+
     Scanner input = new Scanner(System.in);
-    
-    // declare an array of integers called nums of size 100
-    // (to complete)
+
+    int[] nums = new int[100];
+
+    for(int i = 0; i < nums.length; i++)
+    {
+        nums[i] = 100 + i;
+    }
+                    
+    for(int i = 0; i < nums.length; i++)
+    {
+        sum = sum + nums[i];
+        avg1 = sum/nums.length;
+    }
+
+    for(int i = 0; i < nums.length; i++)
+    {
+        nums[i] = nums[i] + 5;
+    }
+
+     for(int i = 0; i < nums.length; i++)
+    {
+        sum = sum +nums[i];
+        avg2 = sum/nums.length;
+    }
    
-
-    // create a for loop that populates array nums such that
-    // the first element is 100, the second element 101, etc.
-    // (to complete)
-    
-    
-    // use a for loop to determine the average of all of the
-    // values in array nums and assigns to variable avg1
-    // (to complete)
-    
-
-    // use a for loop to add 5 to every elements in array nums
-    // (to complete)
-    
-    
-    // use a for loop to determine the average of all of the
-    // values in array nums and assigns to variable avg2
-    // (to complete)
-    
-
-    // display results
     System.out.println("avg1 = " + avg1);
     System.out.println("avg2 = " + avg2);
-    
+
   }
 }
